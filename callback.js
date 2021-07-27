2b41ca280ccba369df0ad0c7b2f116e35af34168
bruku <virtualsavvy.nigeria@gmail.com>
2021-07-27 05:13:00 -0700

    Call back async promise

---
+++ callback.js
@@ -0,0 +1,17 @@
+var posts = [
+    {title: 'Post One', body: 'This is Post One'},
+    {title: 'Post Two', body: 'This is Post Two'},
+
+];
+
+
+function getPost() {
+    setTimeout( function()  {
+        var output = '';
+        posts.forEach(function(post, index) {
+
+        })
+
+    }, 1000); 
+
+}
